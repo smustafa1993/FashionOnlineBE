a4bd9a13afefcef80199620dfb73ca4b9ab44b64
Malak <malakalmahari7042@gmail.com>
2025-07-01 22:47:34 +0300

    added lines to deal with cache issue and added info needed to be used by fe in the payload

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,7 +4,7 @@ const middleware = require('../middleware')
 const Register = async (req, res) => {
   try {
     // Extracts the necessary fields from the request body
-    const { email, password, name } = req.body
+    const { email, password, name, isAdmin, ProfilePic } = req.body
     // Hashes the provided password
     let passwordDigest = await middleware.hashPassword(password)
     // Checks if there has already been a user registered with that email
@@ -13,7 +13,7 @@ const Register = async (req, res) => {
       return res.status(400).send('A user with that email has already been registered!')
     } else {
       // Creates a new user
-      const user = await User.create({ name, email, passwordDigest })
+      const user = await User.create({ name, email, passwordDigest, isAdmin, ProfilePic })
       // Sends the user as a response
       res.status(200).send(user)
     }
@@ -38,7 +38,8 @@ const Login = async (req, res) => {
       let payload = {
         id: user.id,
         email: user.email,
-        isAdmin: user.isAdmin //include the isAdmin field to then use it in checkAdmin
+        isAdmin: user.isAdmin, //include the isAdmin field to then use it in checkAdmin
+        name: user.name
       }
       // Creates our JWT and packages it with our payload to send as a response
       let token = middleware.createToken(payload)
@@ -56,7 +57,7 @@ const UpdatePassword = async (req, res) => {
     // Extracts the necessary fields from the request body
     const { oldPassword, newPassword } = req.body
     // Finds a user by a particular field (in this case, the user's id from the URL param)
-    let user = await User.findById(req.params.user_id)
+    let user = await User.findById(req.params.id)
     // Checks if the password matches the stored digest
     let matched = await middleware.comparePassword(
       oldPassword,
@@ -86,6 +87,9 @@ const UpdatePassword = async (req, res) => {
 
 const CheckSession = async (req, res) => {
   const { payload } = res.locals
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate, max-age=0")
+  res.setHeader("Pragma", "no-cache")
+  res.setHeader("Expires", "0")
   res.status(200).send(payload)
 }
 
